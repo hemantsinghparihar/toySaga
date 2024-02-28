c752f3ab23fc270a23618b1293c08b06aa3216bd
hemant <hemantsinghparihar13@gmail.com>
2024-02-28 10:54:45 +0530

    Installed pg, aws-S3 upload provider and updated the config files

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,22 +1,40 @@
-
-  // strapi-api/config/database.js
-  module.exports = ({ env }) => ({
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
     connection: {
-      client: 'postgres',
-      connection: {
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'toySaga'),
-        user: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', 'hemant@0007'),
-        schema: env('DATABASE_SCHEMA', 'public'), // Not required
-        // ssl: {
-        //   rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
-        // },
-      },
-      debug: false,
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", ""),
+      password: env("DATABASE_PASSWORD", ""),
     },
-  });
+    useNullAsDefault: true,
+  },
+});
+
+  //  strapi-api/config/database.js
+
+  // module.exports = ({ env }) => ({
+  //   connection: {
+  //     client: 'postgres',
+  //     connection: {
+  //       host: env('DATABASE_HOST', 'localhost'),
+  //       port: env.int('DATABASE_PORT', 5432),
+  //       database: env('DATABASE_NAME', 'toySaga'),
+  //       user: env('DATABASE_USERNAME', 'postgres'),
+  //       password: env('DATABASE_PASSWORD', 'hemant@0007'),
+  //       schema: env('DATABASE_SCHEMA', 'public'), // Not required
+  //       // ssl: {
+  //       //   rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+  //       // },
+  //     },
+  //     debug: false,
+  //   },
+  // });
+
+
+
+
   // const path = require('path');
 
 // module.exports = ({ env }) => {
